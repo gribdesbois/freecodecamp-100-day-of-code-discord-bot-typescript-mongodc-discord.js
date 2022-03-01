dbfef7384728538c4175c884d7189415c1488ccf
gribdesbois <gribdesbois@gmail.com>
2022-03-01 15:25:20 +0100

    bot is done. going smooth

--- src/commands/_CommandList.ts
+++ src/commands/_CommandList.ts
@@ -1,6 +1,7 @@
+import { help } from './help'
 import { view } from './view'
 import { edit } from './edit'
 import { CommandInt as Command } from '../interfaces/Command'
 import { oneHundred } from './oneHundred'
 
-export const CommandList: Command[] = [oneHundred, edit, view]
+export const CommandList: Command[] = [oneHundred, edit, view, help]
